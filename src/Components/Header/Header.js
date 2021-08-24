227b3a24d805582034102fe41280b21f616af483
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-08-24 00:38:28 -0500

    trying to change home page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return(
         <>
         <div className = 'topHeader'>
-        <h1>Vacaciones de Locura</h1>
+        <h1>Buena Onda</h1>
         <h2><a href = 'https://www.vrbo.com/3526437ha?adultsCount=6&arrival=2021-10-17&departure=2021-10-21&unitId=3515753'>Our Place</a></h2>
         </div>
         </>
